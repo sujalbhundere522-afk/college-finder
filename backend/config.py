840f81e2ce3ad04029bbf88a228aed90fc5c9e7a
Sujal <sujalbhundere522@gmail.com>
2026-02-24 19:34:18 +0530

    Using environment variables for secrets

--- backend/config.py
+++ backend/config.py
@@ -6,9 +6,9 @@ load_dotenv()
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "collegefinder-super-secure-secret-key-2026-strong-key"
-    JWT_SECRET_KEY = "collegefinder-super-secure-secret-key-2026-strong-key"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
